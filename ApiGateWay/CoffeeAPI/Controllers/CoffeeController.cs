0eef9dd7b822abc730636e80494bc6aea2fa68f8
Abdullah Bahmani <abdolahbahmani@gmail.com>
2021-12-01 14:00:42 +0100

    ok

---
+++ ApiGateWay/CoffeeAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        private static readonly string[] Coffees = new[]
+        {
+            "Flat White", "LongBlack", "Latte", "Americano", "Cappuccino"
+        };
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(Coffees[rng.Next(Coffees.Length)]);
+
+        }
+    }
+
+}
